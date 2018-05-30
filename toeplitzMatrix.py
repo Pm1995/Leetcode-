1c30312a74703f1d2ff16dcf41f6fa8bd21703ef
Pranay Mohanty <PRM58@pitt.edu>
2018-05-30 17:52:29 -0400

    try

---
+++ toeplitzMatrix.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        for i in range(len(matrix)-1):
+            if matrix[i][:-1]!=matrix[i+1][1:]:
+                return False
+        return True 
