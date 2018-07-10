960fbce3b2ccbac9a9ed66905f5739b0a27a9182
Pranay Mohanty <PRM58@pitt.edu>
2018-07-10 14:07:22 -0400

    try

--- leetcode(arrays).py
+++ leetcode(arrays).py
@@ -6,7 +6,7 @@ nums=[0,1,0,3,12]
 
 
 #contain duplicates-Given an array of integers, find if the array contains any duplicates.
-nums=[1,2,3]
+nums=[1,2,3,1]
 dict={}
 for i in range(len(nums)):
 	dict[nums[i]]=0
@@ -21,4 +21,22 @@ for i in dict:
 		break
 	else:
 		continue
+		print('False')
+
+
+
+#majority element-Given an array of size n, find the majority element. The majority element is the element 
+#that appears more than ⌊ n/2 ⌋ times.
+nums=[3,2,3]
+dict={}
+for i in range(len(nums)):
+	dict[nums[i]]=0
+
+for i in nums:
+	if i in dict:
+		dict[i]=dict[i]+1
+for i in dict:
+	if dict[i]>len(nums)/2:
+		print(i)
+
 
