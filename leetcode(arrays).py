37e09febdeb9fadf5f2a20c6b86a8fcc44696407
Pranay Mohanty <PRM58@pitt.edu>
2018-07-10 17:35:03 -0400

    try

--- leetcode(arrays).py
+++ leetcode(arrays).py
@@ -1,10 +1,3 @@
-#move zeros-Given an array nums, write a function to move all 0's 
-#to the end of it while maintaining the relative order of the non-zero elements.
-
-nums=[0,1,0,3,12]
-
-
-
 #contain duplicates-Given an array of integers, find if the array contains any duplicates.
 nums=[1,2,3,1]
 dict={}
@@ -22,6 +15,7 @@ for i in dict:
 	else:
 		continue
 		print('False')
+print("\r")
 
 
 
@@ -38,5 +32,54 @@ for i in nums:
 for i in dict:
 	if dict[i]>len(nums)/2:
 		print(i)
+print("\r")
+
+
+
+#find numbers disappeared in array-Find all the elements of [1, n] inclusive that do not appear in this array.
+nums=[4,3,2,7,8,2,3,1]
+dict={}
+for i in range(len(nums)):
+	dict[nums[i]]=0
+list=[]
+for k in range(max(nums)+1):
+	if k not in dict.keys() and k>0:
+		list.append(k)
+if len(list)>0:
+	print(list)
+print("\r")
+
+
+#Given an array of integers that is already sorted in ascending order, find two numbers such that 
+#they add up to a specific target number.
+nums=[2,7,11,15]
+target = 5
+
+
+list=[]
+for i in range(len(nums)):
+	start=nums[i]
+	for j in range(len(nums)-(i+1)):
+		if start+nums[j+i+1]==target:
+			#print(start,nums[j+1+i])
+			list.append(i+1)
+			list.append(j+i+2)
+
+if len(list)>0:
+	print(list)
+print("\r")
+
+
+
+#Given a matrix A, return the transpose of A.
+matrix=[[1,2,3],[4,5,6]]
+list=[]
+for i in range(len(matrix[0])):
+	trans=[]
+	for j in range(len(matrix)):
+		trans.append(matrix[j][i])
+	list.append(trans)
+print(list)
+print("\r")
 
 
