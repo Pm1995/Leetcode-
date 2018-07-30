e606df1e8bb294c35cb394f6805269e178f3f803
Pranay Mohanty <PRM58@pitt.edu>
2018-07-30 12:18:20 -0400

    ss

--- MissingRepeatedNumbers.py
+++ MissingRepeatedNumbers.py
@@ -21,7 +21,34 @@ for i in range(len(list)):
 	if list[i]>0:
 		new.append(i+1)
 print(new)
-
+print("\r")
 
 #N/3 repeat number 
 A=[1,2,3,1,1]
+dict={}
+for i in range(len(A)):
+	dict[A[i]]=0
+
+for i in range(len(A)):
+	dict[A[i]]=dict[A[i]]+1
+for k in dict:
+	if dict[k]>len(A)/3:
+		print(k)
+print("\r")
+
+#Contains duplicates
+nums=[1,2,1,1]
+dict={}
+for i in range(len(nums)):
+	dict[nums[i]]=0
+for j in range(len(nums)):
+	dict[nums[j]]=dict[nums[j]]+1
+if len(nums)>0:
+	print(max(dict.values())>1)
+else:
+	print(len(nums)==1)
+print("\r")
+
+
+#Contains duplicates 2- Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and 
+#the absolute difference between i and j is at most k.
