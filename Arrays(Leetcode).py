b83be2e695b4d0c8afa36b18e46fa2184b014bab
Pranay Mohanty <PRM58@pitt.edu>
2018-11-10 19:25:55 -0500

    try

--- Arrays(Leetcode).py
+++ Arrays(Leetcode).py
@@ -1,22 +1,3 @@
-#Two Sum 
-nums = [2, 7, 11, 15]
-
-target = 9
-dic={}
-
-for i,n in enumerate(nums):
-	m=target-n
-	dic[m]=i
-
-list=[]
-for l in nums:
-	if l in dic:
-		list.append(dic[l])
-
-print(sorted(list))
-print("\r")
-
-
 #Remove Duplicates from Sorted Array
 nums = [0,0,1,1,1,2,2,3,3,4]
 
@@ -31,24 +12,69 @@ print("\r")
 
 
 #Merge Sorted Array 
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
-n = 3
+nums1 = [1,2,3,9,10]
+nums2 = [2,3,5,6,10,12]
+
+
+a=0
+while a<len(nums1):
+	if nums1[a]==0:
+		nums1.pop(a)
+	else:
+		a=a+1
+
 
 i=0
 j=0
-while i<m and j<n:
-	if nums1[i]<=nums2[j]:
+while i<len(nums1)-1 and j<len(nums2)-1:
+	if nums1[i]<nums2[j]:
 		i=i+1
 	if nums1[i]>nums2[j]:
-		nums1.insert(i-1,nums2[j])
+		nums1.insert(i,nums2[j])
+		nums2.pop(j)
+	if nums1[i]==nums2[j]:
+		nums1.insert(i,nums2[j])
 		nums2.pop(j)
 
-while j<n:
-	if nums1[i]<=nums2[j]:
-		nums1.append(nums2[j])
-		j=j+1
 
+
+while j<len(nums2):
+	nums1.append(nums2[j])
+	nums2.pop(j)
 
 print(nums1)
+print("\r")
+
+
+
+#two sum 
+
+nums = [2, 7, 11, 15]
+target = 9
+dic={}
+
+for i,n in enumerate(nums):
+	m=target-n
+	if m in dic:
+		print(dic[m],i)
+	else:
+		dic[n]=i
+print("\r")
+
+
+#Print true if contains duplicates else print false 
+num=[1,1,1,3,3,4,3,2,4,2]
+d={}
+
+
+for i in num:
+	if i in d:
+		print("true")
+		break
+	else:
+		d[i]=0
+print("\r")
+
+
+
+
