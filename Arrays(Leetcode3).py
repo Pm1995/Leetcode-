69fd2e5dbb6aaefea13c29335d169890fb80b305
Pranay Mohanty <PRM58@pitt.edu>
2019-03-01 10:55:16 -0500

    ss

--- Arrays(Leetcode3).py
+++ Arrays(Leetcode3).py
@@ -262,3 +262,30 @@ print(profit)
 print("\r")
 
 
+#Alice and Bob have candy bars of different sizes: A[i] is the size of the i-th bar of candy that Alice has, 
+#and B[j] is the size of the j-th bar of candy that Bob has.
+#Since they are friends, they would like to exchange one candy bar each so that after the exchange, 
+#they both have the same total amount of candy.  (The total amount of candy a person has is the sum of the sizes of candy bars they have.)
+#Return an integer array ans where ans[0] is the size of the candy bar that Alice must exchange, and ans[1] is the 
+#size of the candy bar that Bob must exchange.
+B=[1,2,5]
+A=[2,4]
+
+
+deficit=abs(sum(A)-sum(B))//2
+
+pos=0
+neg=0
+
+if sum(A)>sum(B):
+	for i in range(len(A)):
+		neg=A[i] - deficit
+		if neg in B:
+			print(neg+deficit,neg)
+elif sum(A)<sum(B):
+	for i in range(len(A)):
+		pos=A[i] + deficit
+		if pos in B:
+			print(pos-deficit,pos)
+
+print("\r")
